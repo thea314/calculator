b83a07447ee62988af2331b0dfb2d60226257a79
Thea Gregory <thea.isis@gmail.com>
2025-02-05 16:09:08 -0500

    completed basic calculator, implementing extended calculations

--- javascript.js
+++ javascript.js
@@ -1,19 +1,25 @@
 // functions for basic arithmetic 
 
 function add(a, b) {
-    return a + b;
+    return Math.round((a + b) * 100) / 100;
 }
 
 function subtract(a, b) {
-    return a - b;
+    return Math.round((a - b) * 100) / 100;
 }
 
 function multiply(a, b) {
-    return a * b;
+    return Math.round((a * b) * 100) / 100;
 }
 
 function divide(a, b) {
-    return a / b;
+
+    if (b === 0) {
+        return 'error'
+    } else {
+        return Math.round((a / b) * 100) / 100;
+    }
+    
 }
 
 // variables for a, b, and operator
@@ -41,9 +47,11 @@ function operate(number1, number2, operator) {
 
 }
 
+//eventListeners for buttons
+
 let displayEquation = document.getElementById('displayArea');
-displayEquation.textContent = "0";
-let equationValue ='0';
+displayEquation.textContent = " ";
+let equationValue =' ';
 
 let one = document.getElementById('one');
 
@@ -51,3 +59,163 @@ one.addEventListener('click', () => {
     equationValue = equationValue += '1';
     displayEquation.textContent = equationValue;
 })
+
+let two = document.getElementById('two');
+
+two.addEventListener('click', () => {
+    equationValue = equationValue += '2';
+    displayEquation.textContent = equationValue;
+})
+
+let three = document.getElementById('three');
+
+three.addEventListener('click', () => {
+    equationValue = equationValue += '3';
+    displayEquation.textContent = equationValue;
+})
+
+let four = document.getElementById('four');
+
+four.addEventListener('click', () => {
+    equationValue = equationValue += '4';
+    displayEquation.textContent = equationValue;
+})
+
+let five = document.getElementById('five');
+
+five.addEventListener('click', () => {
+    equationValue = equationValue += '5';
+    displayEquation.textContent = equationValue;
+})
+
+let six = document.getElementById('six');
+
+six.addEventListener('click', () => {
+    equationValue = equationValue += '6';
+    displayEquation.textContent = equationValue;
+})
+
+let seven = document.getElementById('seven');
+
+seven.addEventListener('click', () => {
+    equationValue = equationValue += '7';
+    displayEquation.textContent = equationValue;
+})
+
+let eight = document.getElementById('eight');
+
+eight.addEventListener('click', () => {
+    equationValue = equationValue += '8';
+    displayEquation.textContent = equationValue;
+})
+
+let nine = document.getElementById('nine');
+
+nine.addEventListener('click', () => {
+    equationValue = equationValue += '9';
+    displayEquation.textContent = equationValue;
+})
+
+let zero = document.getElementById('zero');
+
+zero.addEventListener('click', () => {
+    equationValue = equationValue += '0';
+    displayEquation.textContent = equationValue;
+})
+
+let decimal = document.getElementById('decimal');
+
+decimal.addEventListener('click', () => {
+
+    if (equationValue.endsWith(' ')) {
+        equationValue = equationValue += '0.';
+    } else {
+        equationValue = equationValue += '.';
+    }
+   
+    displayEquation.textContent = equationValue;
+})
+
+let addition = document.getElementById('addition');
+
+addition.addEventListener('click', () => {
+
+    equationTrimmed = equationValue.trimStart();
+
+    if(equationTrimmed.indexOf('+') !== -1) {
+        let equationArray = equationTrimmed.split(' ');
+        displayEquation.textContent = operate(Number(equationArray[0]), Number(equationArray[2]), '+');
+        equationValue = operate(Number(equationArray[0]), Number(equationArray[2]), '+');
+    } else if (equationTrimmed.indexOf('-') !== -1) {
+        let equationArray = equationTrimmed.split(' ');
+        displayEquation.textContent = operate(Number(equationArray[0]), Number(equationArray[2]), '+') + " - ";
+    } else if (equationTrimmed.indexOf('*') !== -1) {
+        let equationArray = equationTrimmed.split(' ');
+        displayEquation.textContent = operate(Number(equationArray[0]), Number(equationArray[2]), '+') + " * ";
+    } else if (equationTrimmed.indexOf('/') !== -1) {
+        let equationArray = equationTrimmed.split(' ');
+        displayEquation.textContent = operate(Number(equationArray[0]), Number(equationArray[2]), '+') + " / ";
+    } else {
+        equationValue = equationValue += ' + ';
+        displayEquation.textContent = equationValue;
+    }
+})
+
+let minus = document.getElementById('minus');
+
+minus.addEventListener('click', () => {
+    equationValue = equationValue += ' - ';
+    displayEquation.textContent = equationValue;
+
+
+})
+
+let multiplication = document.getElementById('multiplication');
+
+multiplication.addEventListener('click', () => {
+    equationValue = equationValue += ' * ';
+    displayEquation.textContent = equationValue;
+
+
+})
+
+let division = document.getElementById('division');
+
+division.addEventListener('click', () => {
+    equationValue = equationValue += ' / ';
+    displayEquation.textContent = equationValue;
+
+})
+
+let CE = document.getElementById('CE');
+
+CE.addEventListener('click', () => {
+    equationValue = ' ';
+    displayEquation.textContent = equationValue;
+
+})
+
+let equals = document.getElementById('equals');
+
+equals.addEventListener('click', () => {
+
+    let equationValueTrimmed = equationValue.trimStart();
+    
+    if (equationValueTrimmed.indexOf('+') !== -1) {       
+        let equationArray = equationValueTrimmed.split(' ');
+        displayEquation.textContent = operate(Number(equationArray[0]), Number(equationArray[2]), '+');
+    }else if (equationValueTrimmed.indexOf('-') !== -1) {
+        let equationArray = equationValueTrimmed.split(' ');
+        displayEquation.textContent = operate(Number(equationArray[0]), Number(equationArray[2]), '-');
+    } else if (equationValueTrimmed.indexOf('*') !== -1) {
+        let equationArray = equationValueTrimmed.split(' ');
+        displayEquation.textContent = operate(Number(equationArray[0]), Number(equationArray[2]), '*');
+    } else if (equationValueTrimmed.indexOf('/') !== -1) {
+        let equationArray = equationValueTrimmed.split(' ');
+        displayEquation.textContent = operate(Number(equationArray[0]), Number(equationArray[2]), '/');
+    } else {
+        displayEquation.textContent = 'error';
+    }
+
+})
+
