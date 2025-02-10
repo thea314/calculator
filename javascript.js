64c029e4eccf53307d14e87b3584dfe1f9c534c7
Thea Gregory <thea.isis@gmail.com>
2025-02-10 14:06:03 -0500

    added event listeners for keyboard, fixed decimal bug, adjusted button size

--- javascript.js
+++ javascript.js
@@ -50,15 +50,106 @@ function operate(number1, number2, operator) {
 //eventListeners for buttons
 
 let displayEquation = document.getElementById('displayArea');
-displayEquation.textContent = " ";
-let equationValue =' ';
+displayEquation.textContent = "";
+let equationValue ='';
 
 let one = document.getElementById('one');
 
 one.addEventListener('click', () => {
+    oneButton();
+})
+
+document.addEventListener('keydown', (e) => {
+    const keyName = e.key;
+
+    if(keyName === "1") {
+        oneButton();
+    } else if (keyName === "2") {
+        twoButton();
+    } else if (keyName === "3") {
+        threeButton();
+    } else if (keyName === "4") {
+        fourButton();
+    } else if (keyName === "5") {
+        fiveButton();
+    } else if (keyName === "6") {
+        sixButton();
+    } else if (keyName === "7") {
+        sevenButton();
+    } else if (keyName === "8") {
+        eightButton();
+    } else if (keyName === "9") {
+        nineButton();
+    } else if (keyName === "0") {
+        zeroButton();
+    } else if (keyName === "+") {
+        additionButton();
+    } else if (keyName === "-") {
+        minusButton();
+    } else if (keyName === "*") { 
+        multiplicationButton();
+    } else if (keyName === "/") {
+        divisionButton();
+    } else if (keyName === "Backspace") {
+        backspaceDeleteButton();
+    } else if (keyName === ".") {
+        decimalButton();
+    } else if (keyName === "Enter" || keyName === "=") {
+        equalsButton();
+    } else if (keyName === "Delete") {
+        CEButton();
+    }
+})
+
+function oneButton() {
     equationValue = equationValue += '1';
     displayEquation.textContent = equationValue;
-})
+}
+
+function twoButton() {
+    equationValue = equationValue += '2';
+    displayEquation.textContent = equationValue;
+}
+
+function threeButton() {
+    equationValue = equationValue += '3';
+    displayEquation.textContent = equationValue;
+}
+
+function fourButton() {
+    equationValue = equationValue += '4';
+    displayEquation.textContent = equationValue;
+}
+
+function fiveButton() {
+    equationValue = equationValue += '5';
+    displayEquation.textContent = equationValue;
+}
+
+function sixButton() {
+    equationValue = equationValue += '6';
+    displayEquation.textContent = equationValue;
+}
+
+function sevenButton() {
+    equationValue = equationValue += '7';
+    displayEquation.textContent = equationValue;
+}
+
+function eightButton() {
+    equationValue = equationValue += '8';
+    displayEquation.textContent = equationValue;
+}
+
+function nineButton() {
+    equationValue = equationValue += '9';
+    displayEquation.textContent = equationValue;
+}
+
+function zeroButton() {
+    equationValue = equationValue += '0';
+    displayEquation.textContent = equationValue;
+}
 
 let two = document.getElementById('two');
 
@@ -125,21 +216,31 @@ zero.addEventListener('click', () => {
 
 let decimal = document.getElementById('decimal');
 
-decimal.addEventListener('click', () => {
+function decimalButton() {
 
-    if (equationValue.endsWith(' ')) {
+    if (equationValue.indexOf('.') !== -1) {
+        equationValue = equationValue += '';
+    }else if (equationValue.endsWith(' ')) {
         equationValue = equationValue += '0.';
     } else {
         equationValue = equationValue += '.';
     }
    
     displayEquation.textContent = equationValue;
+}
+
+decimal.addEventListener('click', () => {
+    decimalButton();
+    
 })
 
 let addition = document.getElementById('addition');
 
 addition.addEventListener('click', () => {
+    additionButton();    
+})
 
+function additionButton() {
     equationTrimmed = equationValue.trim();
     let equationArray = equationTrimmed.split(' ');
 
@@ -191,11 +292,15 @@ addition.addEventListener('click', () => {
         equationValue = equationValue += ' + ';
         displayEquation.textContent = equationValue;
     }
-})
+}
 
 let minus = document.getElementById('minus');
 
 minus.addEventListener('click', () => {
+    minusButton();
+})
+
+function minusButton() {
     equationTrimmed = equationValue.trim();
     let equationArray = equationTrimmed.split(' ');
 
@@ -248,12 +353,15 @@ minus.addEventListener('click', () => {
         displayEquation.textContent = equationValue;
     }
 
-
-})
+}
 
 let multiplication = document.getElementById('multiplication');
 
 multiplication.addEventListener('click', () => {
+    multiplicationButton();
+})
+
+function multiplicationButton() {
     equationTrimmed = equationValue.trim();
     let equationArray = equationTrimmed.split(' ');
 
@@ -306,12 +414,15 @@ multiplication.addEventListener('click', () => {
         displayEquation.textContent = equationValue;
     }
 
-
-})
+}
 
 let division = document.getElementById('division');
 
 division.addEventListener('click', () => {
+    divisionButton();
+})
+
+function divisionButton() {
     equationTrimmed = equationValue.trim();
     let equationArray = equationTrimmed.split(' ');
 
@@ -363,21 +474,27 @@ division.addEventListener('click', () => {
         equationValue = equationValue += ' / ';
         displayEquation.textContent = equationValue;
     }
-
-})
+}
 
 let CE = document.getElementById('CE');
 
 CE.addEventListener('click', () => {
-    equationValue = ' ';
-    displayEquation.textContent = equationValue;
+    CEButton();
 
 })
+
+function CEButton() {
+    equationValue = ' ';
+    displayEquation.textContent = equationValue;
+}
 
 let equals = document.getElementById('equals');
 
 equals.addEventListener('click', () => {
+    equalsButton();
+})
 
+function equalsButton() {
     let equationValueTrimmed = equationValue.trimStart();
     
     if (equationValueTrimmed.indexOf('+') !== -1) {       
@@ -399,14 +516,16 @@ equals.addEventListener('click', () => {
     } else {
         displayEquation.textContent = 'error';
     }
-
-})
+}
 
 let backspaceDelete = document.getElementById('backspace');
-
+    
 backspaceDelete.addEventListener('click', () => {
-
-    let shorterEquation = equationValue.substring(0, equationValue.length -1);
-    equationValue = shorterEquation;
-    displayEquation.textContent = equationValue;
+    backspaceDeleteButton();
 })
+
+function backspaceDeleteButton() {
+        let shorterEquation = equationValue.substring(0, equationValue.length -1);
+        equationValue = shorterEquation;
+        displayEquation.textContent = equationValue;
+}
